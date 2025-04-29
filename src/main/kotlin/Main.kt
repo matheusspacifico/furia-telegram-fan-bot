15608adc0d5601f8bf8db7319c304256e9cb6318
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-29 19:19:33 -0300

    feat: add bot initializer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,17 @@
 package com.furia
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import com.furia.com.furia.config.EnvConfig
+import com.furia.com.furia.handler.handleStartCommand
+import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    val bot = bot {
+        token = EnvConfig.BOT_TOKEN
+        dispatch {
+            handleStartCommand()
+        }
     }
+
+    bot.startPolling()
 }
