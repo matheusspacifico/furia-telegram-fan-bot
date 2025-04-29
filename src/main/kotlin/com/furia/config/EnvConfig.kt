014508ca916f46e8684c9b24690592ed4e008268
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-29 02:31:59 -0300

    feat: add env configuration file

---
+++ src/main/kotlin/com/furia/config/EnvConfig.kt
@@ -0,0 +1,12 @@
+package com.furia.com.furia.config
+
+import io.github.cdimascio.dotenv.dotenv
+
+object EnvConfig {
+    private val dotenv = dotenv {
+        ignoreIfMissing = true
+    }
+
+    val BOT_TOKEN = dotenv["BOT_TOKEN"] ?: throw IllegalArgumentException("Missing bot token in .env!")
+    val API_URL = dotenv["API_URL"] ?: "hltv.org"
+}
