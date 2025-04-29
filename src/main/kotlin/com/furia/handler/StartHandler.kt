e9d37701fb66b702fd346892fd83cd7695ce133b
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-29 19:19:41 -0300

    feat: add starthandler structure

---
+++ src/main/kotlin/com/furia/handler/StartHandler.kt
@@ -0,0 +1,10 @@
+package com.furia.com.furia.handler
+
+import com.github.kotlintelegrambot.dispatcher.Dispatcher
+import com.github.kotlintelegrambot.dispatcher.command
+
+fun Dispatcher.handleStartCommand() {
+    command("start") {
+
+    }
+}
