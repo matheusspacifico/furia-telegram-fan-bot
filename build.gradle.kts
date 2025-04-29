4df9c834b0e8383d56694881b04e2ac783914811
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-29 02:31:49 -0300

    build: add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,17 @@ repositories {
 }
 
 dependencies {
+    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.3.0")
+
+    implementation("io.ktor:ktor-client-core:3.1.2")
+    implementation("io.ktor:ktor-client-cio:3.1.2")
+
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+    implementation("ch.qos.logback:logback-classic:1.5.13")
+
     testImplementation(kotlin("test"))
 }
 
